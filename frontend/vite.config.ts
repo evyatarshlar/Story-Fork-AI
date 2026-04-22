f94c2a274375b9a88668ac48fd1556767886264d
evyatar shlar <evyatar.szlar@gmail.com>
2026-04-22 15:09:29 +0300

    feat: Load environment variables in Vite configuration and update server proxy settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false
-      }
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  console.log(env.VITE_DEBUG)
+
+  return {
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] })
+    ],
+    server: {
+      ...(env.VITE_DEBUG === "true" && {
+        proxy: {
+          "/api": {
+            target: "http://localhost:8000",
+            changeOrigin: true,
+            secure: false
+          }
+        }
+      })
     }
   }
 })
